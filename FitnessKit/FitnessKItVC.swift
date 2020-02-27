0dc60125f8cbe9b4ccc6b77c2a85da9b6d8d71df
sitnikov <oleg.sitnikov@icloud.com>
2020-02-27 13:28:45 +0300

    Initial Commit

---
+++ FitnessKit/FitnessKItVC.swift
@@ -0,0 +1,20 @@
+//
+//  FitnessKItVC.swift
+//  FitnessKit
+//
+//  Created by Oleg Sitnikov on 27.02.2020.
+//  Copyright © 2020 Oleg Sitnikov. All rights reserved.
+//
+
+import UIKit
+
+class FitnessKItVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
